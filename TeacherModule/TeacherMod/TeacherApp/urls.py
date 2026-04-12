540aec2eec5b778273686253e9663fc6524c3748
Ayush-OP00 <allinone7925@gmail.com>
2026-04-12 11:41:32 +0530

    handeled some issues with the urls.py to open first page to login page.

--- TeacherModule/TeacherMod/TeacherApp/urls.py
+++ TeacherModule/TeacherMod/TeacherApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.teacher_dashboard), #http://127.0.0.1:8000/ this will work 
+    path('',views.login_view), #http://127.0.0.1:8000/ this will work 
     path('dashboard/', views.teacher_dashboard, name='teacher_dashboard'), #http://127.0.0.1:8000/dashboard/ = both will open the same page
     path('login/', views.login_view, name = 'login'),
 ]
